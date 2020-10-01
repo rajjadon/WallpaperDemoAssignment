364c1b8730c989f0489383dbcd8f0354aec41be6
Raj pratap singh jadon <pratap.rajjadon07@gmail.com>
2020-10-01 10:31:56 +0530

    final touch up

--- app/src/main/java/org/school/demoapp/ui/splash/MainActivity.kt
+++ app/src/main/java/org/school/demoapp/ui/splash/MainActivity.kt
@@ -1,17 +1,22 @@
 package org.school.demoapp.ui.splash
 
+import android.app.AlertDialog
+import android.content.Context
 import android.content.Intent
+import android.net.ConnectivityManager
 import android.os.Bundle
 import android.os.Handler
+import android.util.EventLogTags
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
-import com.squareup.picasso.Picasso
 import org.school.demoapp.R
 import org.school.demoapp.databinding.ActivityMainBinding
 import org.school.demoapp.ui.homeScreen.WallpapersHomeScreen
 import java.util.*
+
 
 class MainActivity : AppCompatActivity()
 {
@@ -21,23 +26,32 @@ class MainActivity : AppCompatActivity()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         activityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
-        mainViewModel = ViewModelProvider(this).get(MainViewModel::class.java)
 
-        setHandler()
-
-        if ( ::mainViewModel.isInitialized )
+        if ( isOnline() )
         {
-            val random = Random()
-            val randomIndex: Int = random.nextInt(10 ) + 1
-            mainViewModel.wallpapers.observe(this, Observer {
+            mainViewModel = ViewModelProvider(this).get(MainViewModel::class.java)
 
-                it?.wallPaperLists?.get( randomIndex )?.largeImageURL?.let { image ->
+            setHandler()
 
-                    activityMainBinding.imageUrl = image
-                    //Picasso.get().load(image).into(activityMainBinding.image)
+            if ( ::mainViewModel.isInitialized )
+            {
+                val random = Random()
+                val randomIndex: Int = random.nextInt(10) + 1
+                mainViewModel.wallpapers.observe(this, Observer {
 
-                }
-            })
+                    it?.wallPaperLists?.get(randomIndex)?.largeImageURL?.let { image ->
+
+                        activityMainBinding.imageUrl = image
+                        //Picasso.get().load(image).into(activityMainBinding.image)
+
+                    }
+                })
+            }
+        }
+        else
+        {
+            activityMainBinding.includeNoData.tvNoData.text = "No internet connection "
+            activityMainBinding.includeNoData.noData.visibility = View.VISIBLE
         }
     }
 
@@ -50,4 +64,15 @@ class MainActivity : AppCompatActivity()
         }
         handler.postDelayed(r, 3000)
     }
+
+    fun isOnline(): Boolean {
+        val cm = getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        val netInfo = cm.activeNetworkInfo
+        if (netInfo != null && netInfo.isConnectedOrConnecting) {
+            return true
+        }
+        return false
+    }
+
+
 }
